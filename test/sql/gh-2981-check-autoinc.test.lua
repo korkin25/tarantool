96975db4f246bc09a3801dd247f5ab65d7709a6f
Mergen Imeev <imeevma@gmail.com>
2019-07-03 14:05:11 +0300

    sql: remove unnecessary AUTOINCREMENT ID generation
    Currently, if we perform something like
    CREATE TABLE t (i INT PRIMARY KEY AUTOINCREMENT);
    INSERT INTO t(a) VALUES (NULL);
    
    we generate a new identifier in a special way. This was necessary
    for the cases described in issue #2981. Here it is:
    CREATE TABLE t1 (
            s1 INTEGER PRIMARY KEY AUTOINCREMENT,
            s2 INTEGER,
            CHECK (s1 <> 19)
    );
    INSERT INTO t1 VALUES (18, NULL);
    INSERT INTO t1 (s2) VALUES (NULL);
    
    In this case, the CHECK did not work properly. This is not
    necessary now, because CHECK was moved to BOX due to issue #3691.
    This patch removes the mentioned special way.
    
    Part of #4188

--- test/sql/gh-2981-check-autoinc.test.lua
+++ test/sql/gh-2981-check-autoinc.test.lua
@@ -7,6 +7,8 @@ box.cfg{}
 box.execute("CREATE TABLE t1 (s1 INTEGER PRIMARY KEY AUTOINCREMENT, s2 INTEGER, CHECK (s1 <> 19));");
 box.execute("CREATE TABLE t2 (s1 INTEGER PRIMARY KEY AUTOINCREMENT, s2 INTEGER, CHECK (s1 <> 19 AND s1 <> 25));");
 box.execute("CREATE TABLE t3 (s1 INTEGER PRIMARY KEY AUTOINCREMENT, s2 INTEGER, CHECK (s1 < 10));");
+box.execute("CREATE TABLE t4 (s1 INTEGER PRIMARY KEY AUTOINCREMENT, s2 INTEGER, CHECK (s1 < 10));");
+box.execute("CREATE TABLE t5 (s1 INTEGER PRIMARY KEY AUTOINCREMENT, s2 INTEGER, CHECK (s1 <> 19));");
 
 box.execute("insert into t1 values (18, null);")
 box.execute("insert into t1(s2) values (null);")
@@ -19,7 +21,14 @@ box.execute("insert into t2(s2) values (null);")
 box.execute("insert into t3 values (9, null)")
 box.execute("insert into t3(s2) values (null)")
 
+box.execute("insert into t4 values (9, null)")
+box.execute("insert into t4 values (null, null)")
+
+box.execute("INSERT INTO t5 VALUES (18, NULL);")
+box.execute("INSERT INTO t5 SELECT NULL, NULL FROM t5;")
+
 box.execute("DROP TABLE t1")
 box.execute("DROP TABLE t2")
 box.execute("DROP TABLE t3")
-
+box.execute("DROP TABLE t4")
+box.execute("DROP TABLE t5")
